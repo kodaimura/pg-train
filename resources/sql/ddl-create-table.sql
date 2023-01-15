0249ee5ed55afab4ba1c2dc8c837830b49e72b0e
kodaimura <kodaimura123@gmail.com>
2023-01-15 17:35:56 +0900

    add create-table-ddl

---
+++ resources/sql/ddl-create-table.sql
@@ -0,0 +1,53 @@
+CREATE TABLE IF NOT EXISTS users (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	username TEXT NOT NULL UNIQUE,
+	password TEXT NOT NULL,
+	create_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+	update_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+);
+
+CREATE TABLE IF NOT EXISTS questions (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	statement TEXT NOT NULL,
+	hint TEXT,
+	answer TEXT NOT NULL,
+	level INTEGER,
+	respondents INTEGER NOT NULL DEFAULT 0,
+	classifying_id TEXT NOT NULL DEFAULT '00',
+	del_flg TEXT NOT NULL DEFAULT '0',
+	create_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+	update_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+);
+
+CREATE TABLE IF NOT EXISTS answers (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	user_id INTEGER NOT NULL,
+	correct_flg TEXT NOT NULL DEFAULT '0',
+	program TEXT,
+	help_flg TEXT NOT NULL DEFAULT '0',
+	advice TEXT,
+	create_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+	update_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+);
+
+CREATE TRIGGER IF NOT EXISTS trg_users_upd AFTER UPDATE ON users
+BEGIN
+	UPDATE users
+	SET update_at = DATETIME('now', 'localtime')
+	WHERE rowid == NEW.rowid;
+END;
+
+CREATE TRIGGER IF NOT EXISTS trg_questions_upd AFTER UPDATE ON questions
+BEGIN
+	UPDATE questions
+	SET update_at = DATETIME('now', 'localtime')
+	WHERE rowid == NEW.rowid;
+END;
+
+CREATE TRIGGER IF NOT EXISTS trg_answers_upd AFTER UPDATE ON answers
+BEGIN
+	UPDATE answers
+	SET update_at = DATETIME('now', 'localtime')
+	WHERE rowid == NEW.rowid;
+END;
+
--- src/pg_train/db.clj
+++ src/pg_train/db.clj
@@ -5,15 +5,3 @@
 
 (def db {:dbtype "sqlite"
          :dbname "db/pg-train.db"})
-
-
-(defn create-users-table! []
-  (let [sql "create table if not exists users (
-              id integer primary key autoincrement,
-              username text unique,
-              password text,
-              created_at text default (datetime('now','localtime')),
-              updated_at text default (datetime('now','localtime')))"]
-    (sql/query db [sql])))
-
-(create-users-table!)

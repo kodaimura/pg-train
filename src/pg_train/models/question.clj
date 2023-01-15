65b267417b039c1ad298a0abda684a9489c54d5c
kodaimura <kodaimura123@gmail.com>
2023-01-15 22:35:43 +0900

    add models.question

---
+++ src/pg_train/models/question.clj
@@ -0,0 +1,78 @@
+(ns pg-train.models.question
+  (:require
+    [next.jdbc.sql :as sql]
+    [pg-train.db :refer [db]]))
+
+
+(defn insert!
+  [question]
+  (let [sql [
+         "insert into questions (
+            statement, 
+            hint, 
+            answer, 
+            level, 
+            respondents, 
+            classifying_id
+          ) values (?, ?, ?, ?, ?, ?)"
+         (:statement question)
+         (:hint question)
+         (:answer question)
+         (:level question)
+         (:respondents question)
+         (:classifying_id question)]]
+     (sql/query db sql)))
+
+(defn update!
+  [question]
+  (let [sql [
+         "update questions
+          set
+            statement = ?,
+            hint = ?,
+            answer = ?,
+            level = ?,
+            respondents = ?,
+            classifying_id = ?,
+            del_flg = ?
+          where id = ?"
+         (:statement question)
+         (:hint question)
+         (:answer question)
+         (:level question)
+         (:respondents question)
+         (:classifying_id question)
+         (:del_flg question)
+         (:id question)]]
+     (sql/query db sql)))
+
+(defn select-by-id
+  [id]
+  (let [sql [
+         "select 
+            statement, 
+            hint, 
+            answer, 
+            level, 
+            respondents, 
+            classifying_id, 
+            del_flg
+          from questions
+          where id = ?"
+          id]]
+    (sql/query db sql)))
+
+(defn select-all
+  []
+  (let [sql [
+         "select
+            statement, 
+            hint, 
+            answer, 
+            level, 
+            respondents, 
+            classifying_id, 
+            del_flg
+          from questions
+          where del_flg = '0'"]]
+    (sql/query db sql)))
